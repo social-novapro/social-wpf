2d306c2dc02169ace4b874b69943f3ac054e868b
Daniel Kravec <daniel@kravec.com>
2026-04-26 22:59:24 +0100

    Add project files.

---
+++ social-wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace social_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
